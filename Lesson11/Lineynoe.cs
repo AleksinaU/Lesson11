d99073351f64f99e09b66029e987be89ccdbc2eb
Юлия Алексина <ulchez@mail.ru>
2022-07-27 15:50:49 +0300

    Добавьте файлы проекта.

---
+++ Lesson11/Lineynoe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    struct Lineynoe
+    {
+        double k;
+        double b;
+        public Lineynoe(double k, double b)
+        {
+        this.k = k;
+        this.b = b;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            if (b == 0 & k == 0)
+                return "Решений бесконечное множество";
+            if (b != 0 & k == 0)
+                return "У уровнения не может быть корней";
+            else
+                return $"Корень уравнения равен {x}";
+        }
+    }
+}
---
+++ Lesson11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте решим линейное уравнение.");
+            Console.WriteLine("Введите число k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Lineynoe lineynoe = new Lineynoe(k, b);
+            Console.WriteLine(lineynoe.Root());
+            Console.ReadKey();
+        }
+    }
+}
